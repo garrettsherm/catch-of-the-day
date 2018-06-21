f833f73157457a181cad68f5f2e5b7260c1075fe
Garrett S <garrettsherm@gmail.com>
2018-06-21 19:42:07 -0400

    finished lesson 10

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react';
-
+import { getFunName } from '../helpers'
 class StorePicker extends React.Component {
 	render(){
 		return (
 				<form action="" className="store-selector">
 					{ /*comment*/ }
 					<h2>Please Enter A Store</h2>
-					<input type="text" placeholder="Store Name" required/>
+					<input type="text" placeholder="Store Name" defaultValue={getFunName()} required/>
 					<button>Visit Store</button> 
 				</form>
 		);
