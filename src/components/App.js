272251bdedeeaa0ad81bf7cb5bb241058b98d344
Garrett S <garrettsherm@gmail.com>
2018-06-22 16:44:41 -0400

    finished lesson 13

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,31 @@ import Inventory from './Inventory';
 
 
 class App extends React.Component {
+	
+	state = {
+		fishes: {},
+		order: {}
+	};
+
+	addFish = (fish) => {
+		// take copy of state
+		const fishes = { ...this.state.fishes };
+		// add new fish to copy
+		fishes[`fish${Date.now()}`] = fish;
+		//set new fishes obj to state
+		this.setState({
+			fishes: fishes
+		});
+	};
+
 	render() {
 		return(
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market" />
 				</div>
-				<Inventory />
 				<Order /> 
+				<Inventory addFish={this.addFish} />
 			</div>
 		);
 	}
