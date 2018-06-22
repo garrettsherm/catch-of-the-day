8ac24a8f16364a65496c9af21ad6ec716ba018e3
Garrett S <garrettsherm@gmail.com>
2018-06-22 14:35:18 -0400

    finished lesson 11

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,36 @@
 import React, { Fragment } from 'react';
 import { getFunName } from '../helpers'
 class StorePicker extends React.Component {
+	
+	myInput = React.createRef();
+
+	/*
+	// constructor using es6
+	constructor(){
+		super();
+		this.goToStore = this.goToStore.bind(this);	
+	}
+	*/
+
+	//alternative to constuctor
+
+
+	goToStore = event => {
+		event.preventDefault();
+	};
+
 	render(){
 		return (
-				<form action="" className="store-selector">
+				<form className="store-selector" onSubmit={this.goToStore}>
 					{ /*comment*/ }
 					<h2>Please Enter A Store</h2>
-					<input type="text" placeholder="Store Name" defaultValue={getFunName()} required/>
-					<button>Visit Store</button> 
+					<input 
+						type="text" 
+						placeholder="Store Name" 
+						defaultValue={getFunName()} 
+						required 
+					/>
+					<button type="submit">Visit Store</button> 
 				</form>
 		);
 	}
