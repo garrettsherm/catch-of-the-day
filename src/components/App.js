38aff7c7b859d47019dbc00d819ffe2a8eb42c92
Garrett S <garrettsherm@gmail.com>
2018-06-25 22:01:36 -0400

    finished lesson 19

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,10 @@ class App extends React.Component {
 
 	componentDidMount(){
 		const { params } = this.props.match
+		const localStorageRef = localStorage.getItem(params.storeId);
+		if(localStorageRef){
+			this.setState({ order: JSON.parse(localStorageRef) });
+		}
 		this.ref = base.syncState(`${params.storeId}/fishes`, {
 			context: this,
 			state: 'fishes'
@@ -23,7 +27,11 @@ class App extends React.Component {
 
 	componentWillUnmount(){
 		base.removeBinding(this.ref);
-	}
+	};
+
+	componentDidUpdate(){
+		localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
+	};
 
 	addFish = (fish) => {
 		// take copy of state
